17030fcee6e93bd295c065e5a608a9a420fae7b8
vittmlb <vittmlb@users.noreply.github.com>
2018-12-08 00:27:59 -0200

    outro teste

--- dist/cns.js
+++ dist/cns.js
@@ -9,8 +9,8 @@ program
 program
     .command('sem [fileName]')
     .action(async (fileName) => {
-    let filePath = path.join(__dirname, 'data', fileName);
-    let csv = new class_convert_to_degust_1.ConvertToDegust(filePath);
+    // let filePath = path.join(__dirname, 'data', fileName);
+    let csv = new class_convert_to_degust_1.ConvertToDegust(fileName);
     csv.parseData();
     csv.writeFile(path.join(destinationFolder, 'contagem_semanal_40.csv'));
 });
